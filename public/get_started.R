6bf878a04c0d117f8bc256c075f4c571fe77e460
Gabi Huiber <ghuiber@gmail.com>
2017-09-04 00:33:26 -0700

    OK, that's enough for a start.

--- public/get_started.R
+++ public/get_started.R
@@ -7,4 +7,9 @@ blogdown::install_hugo()
 
 # I am already in the project rblog, so all good:
 blogdown::new_site()
+
+# Here's a way to install a different theme
+# but you'll have to fix it up by hand. In
+# particular, you'll have to get MathJax 
+# to work:
 blogdown::install_theme('calintat/minimal')
